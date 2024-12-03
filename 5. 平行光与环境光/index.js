24b53b916fdbfe1fcb1db03788196ad481fdc0d0
Linya <cdthanye32@qq.com>
2024-12-03 08:09:42 +0800

    feat: 【5. 平行光与环境光】添加不同光源测试代码

--- 5. 平行光与环境光/index.js
+++ 5. 平行光与环境光/index.js
@@ -1,15 +1,30 @@
-import {camera, scene, renderer} from "lib/scene/basic.js"
+import {camera, scene, renderer, mesh} from "lib/scene/basic.js"
 import * as THREE from "three";
 
-const pointLight = new THREE.PointLight(0xffffff, 1);
-const pointLightHelper = new THREE.PointLightHelper(pointLight, 10)
+const pointLight = new THREE.PointLight(0xffffff, 40000)
+pointLight.position.set(400, 20, 20)
+const pointLightHelper = new THREE.PointLightHelper(pointLight)
 
-pointLight.position.set(400, 200, 200);
-pointLight.intensity = 4000;
-pointLight.decay = 1
+// 环境光
+const ambient = new THREE.AmbientLight(0xffffff, 1)
 
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
+directionalLight.position.set(100, 100, 100)
+// NOTE: 对比不同入射角，mesh表面对光照的反射效果
+directionalLight.position.set(100, 0, 0);
+directionalLight.position.set(0, 100, 0);
+directionalLight.position.set(100, 100, 100);
+directionalLight.position.set(100, 60, 50);
+//directionalLight.target默认指向坐标原点
+directionalLight.target = mesh
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight)
+
+scene.add(ambient)
 scene.add(pointLight)
-scene.add(pointLightHelper)
+// scene.add(pointLightHelper)
+scene.add(directionalLight)
+scene.add(directionalLightHelper)
 
 renderer.render(scene, camera);
 document.body.appendChild(renderer.domElement);
