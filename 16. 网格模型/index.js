58b1ebfe568e1ba8b584dbd257b5fd9acda01151
Linya <cdthanye32@qq.com>
2024-12-11 21:31:20 +0800

    feat: [16. 网格模型] init

---
+++ 16. 网格模型/index.js
@@ -0,0 +1,15 @@
+import {camera, scene, renderer, mesh} from "lib/scene/basic.js"
+import * as THREE from "three";
+
+const pointLight = new THREE.PointLight(0xffffff, 40000)
+pointLight.position.set(400, 200, 200)
+const pointLightHelper = new THREE.PointLightHelper(pointLight)
+
+const ambient = new THREE.AmbientLight(0xffffff, 0.5)
+
+// 环境光
+scene.add(pointLight)
+scene.add(ambient)
+
+renderer.render(scene, camera);
+document.body.appendChild(renderer.domElement);
